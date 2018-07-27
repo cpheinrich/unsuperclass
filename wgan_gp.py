37cf54e9df5156d6bb6356647138bdbe7454189e
Chris Heinrich <cpheinrich@gmail.com>
2018-07-27 11:47:12 -0700

    Updated to pdf

--- wgan_gp.py
+++ wgan_gp.py
@@ -245,7 +245,7 @@ class WGANGP():
                 #axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
                 axs[i,j].axis('off')
                 cnt += 1
-        fig.savefig("images/cifar_%d.png" % epoch)
+        fig.savefig("images/cifar_%d.pdf" % epoch)
         plt.close()
 
 
